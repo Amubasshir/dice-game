76648a0520cbabf685468f58d327ef6e85b99791
Mubasshir Ahmed <lastmubasshir@gmail.com>
2022-08-20 10:21:51 +0600

    random dice roll added

---
+++ script.js
@@ -0,0 +1,28 @@
+// Selecting Elements
+const score0EL = document.querySelector('#score--0');
+const score1EL = document.querySelector('#score--1');
+const current0EL = document.querySelector('#current--0');
+const current1EL = document.querySelector('#current--1');
+const diceEL = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Starting Condition
+score0EL.textContent = 0;
+score1EL.textContent = 0;
+diceEL.classList.add('hidden');
+let currentScore = 0;
+let activePlayer = 1;
+// Rolling Dice Functionality
+btnRoll.addEventListener('click', function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  diceEL.classList.remove('hidden');
+  diceEL.src = `images /dice-${dice}.png`;
+  if (dice !== 1) {
+    currentScore += dice;
+    current0EL.textContent = currentScore;
+  } else {
+  }
+});
